9beb754696fc7161fca31d042a150bb0e43ad9dd
unknown <767573740@qq.com>
2019-03-21 15:43:23 +0800

    first commit

---
+++ base_type.py
@@ -0,0 +1,64 @@
+
+# 声明一个int_demo 方法
+# def int_demo():
+    #  声明aint变量 ，并赋值 1
+    # aint = 1
+    # 打印 aint 的值
+    # print(aint)
+    # 打印 aint 的类型； type（aint)：获取aint的类型
+    # int_demo()
+# 声明一个 add 方法 参数 有两个 aint bint
+# def add(aint,bint):
+    # 打印aint参数
+    # print(aint)
+    # 打印bint参数
+    # print(bint)
+    # 返回 aint + bint
+    # return aint+bint
+
+#def float_demo():
+   # afloat = 1.90
+   # print( afloat)
+    # print(type(afloat))
+
+# def str_demo():
+    # c = 'hellow'
+    # d = 'world'
+    # return c+d
+
+# if __name__ == '__main__':
+   # print (str_demo())
+    # int_demo()
+    # 提取变量 cart+alt+V
+    # 调用add方法 传入1 2得到返回值，赋予给result变量值
+    # result = add(1.1,2.2)
+    #float_demo()
+    # print(float)
+
+def str_demo2():
+    a= 'hello '
+    b= 250
+    print('a=%s b=%s'%(a,b))
+    s = str(b)
+    print(s)
+    print(type(s))
+    print(a+s)
+
+if __name__ == '__main__':
+    str_demo2()
+
+k
+
+
+
+
+
+
+
+
+
+
+
+
+
+
