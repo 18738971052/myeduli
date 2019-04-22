5b24efcaf8f4b7d878615f08d6d6ad1687c35215
李静伟 <767573740@qq.com>
2019-04-22 15:10:36 +0800

    修正

---
+++ 练习.py
@@ -0,0 +1,27 @@
+
+def int_demo():
+    aint = 1
+    print(aint)
+    print(type(aint))
+
+def str_demo():
+    astr = '1'
+    print(astr)
+    print(type(astr))
+
+
+
+
+
+
+if __name__ == '__main__':
+    print(int_demo())
+    # print(str_demo())
+
+
+
+
+
+
+
+
